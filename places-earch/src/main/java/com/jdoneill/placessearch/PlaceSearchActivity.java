7ea481ca5906ed55be635a799bcc98a1b6e254af
Dan O'Neill <dev@jdoneill.com>
2018-02-28 13:44:10 -0800

    start activity with search view open

--- places-earch/src/main/java/com/jdoneill/placessearch/PlaceSearchActivity.java
+++ places-earch/src/main/java/com/jdoneill/placessearch/PlaceSearchActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -80,6 +81,9 @@ public class PlaceSearchActivity extends AppCompatActivity implements PlacesList
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.placeSearch);
+        searchMenuItem.expandActionView();
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.placeSearch).getActionView();
